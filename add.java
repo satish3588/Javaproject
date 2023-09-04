964fdf845d976039a4bc9c5e67337e40d76cccde
satish kamble <satish kamble>
2023-09-04 17:44:54 +0530

    this my first commit

---
+++ add.java
@@ -0,0 +1,28 @@
+ class A
+{}[D
+
+
+[A[A[A[C
+
+
+[D[D[A[A[A[C
+[D[C[A 
+public static void main()
+{}[D
+[D[D[D[D[D[D[D[D[B[A[C
+
+
+
+
+
+
+
+
+
+
+
+
+[A[A[A[A[A[A[B[A[A[A[B[B[B[D[D[D[D[D[D[D[D[D[D[D}[A[A[A[A[A[A[A[A  
+int a=20;
+int b=30;
+int c=5}hiii..
